dfbebc2f25d6861194dd1ae35124a52992301d1b
Nigus, Hafte <hnigus@oeconnection.com>
2023-07-06 16:05:26 +0300

    leetcodechallenges June 06 and 07 2023

---
+++ LeetCode.Problems/ReverseLinkedList.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.Problems
+{
+    /// <summary>
+    /// 206. Reverse Linked List
+    /// Given the head of a singly linked list, reverse the list, and return the reversed list.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class ReverseLinkedList<T>
+    {
+        public static ListNode<T> Reverse(ListNode<T> head)
+        {
+            ListNode<T> node = head;
+            ListNode<T> prev = null;
+            while (node != null)
+            {
+                ListNode<T> next = node.Next;
+                node.Next = prev;
+                prev = node;
+                node = next;
+            }
+            return prev;
+        }
+
+        public static void Print(ListNode<T> node)
+        {
+            while (node != null)
+            {
+                node.Print();
+                node = node.Next;
+            }
+        }
+    }
+}
+
+// Definition for singly - linked list.
+public class ListNode<T>
+{
+    public T Value;
+    public ListNode<T> Next;
+    public ListNode(T val = default(T), ListNode<T> next = null)
+    {
+        this.Value = val;
+        this.Next = next;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(this.Value.ToString());
+    }
+}
+
---
+++ LeetCode/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode.Problems;
+
+Console.WriteLine("# Of Iselands!");
+
+
+char[][] grid = {
+    new char[] { '1', '1', '1', '1', '0' },
+    new char[] { '1', '1', '0', '1', '0' },
+    new char[] { '1', '1', '0', '0', '0' },
+    new char[] { '0', '0', '0', '0', '0' }
+};
+
+char[][] grid2 = {
+    new char[] { '1','1','0','0','0' },
+    new char[] { '1','1','0','0','0' },
+    new char[] { '0','0','1','0','0' },
+    new char[] { '0','0','0','1','1' }
+};
+
+
+int numberOfIselands = new NumberOfIselands().FindNumberOfIselands(grid);
+Console.WriteLine(numberOfIselands);
+numberOfIselands = new NumberOfIselands().FindNumberOfIselands(grid2);
+Console.WriteLine(numberOfIselands);
+
+
+Console.WriteLine("Pascals Traingle!");
+var a = new PascalsTraingle().ConstructPascalsTraingle(5);
+
+
+Console.WriteLine("Contains Duplicate!");
+int[][] numsAarr = new int[][] {
+    new int[] { 1, 2, 3, 1 },
+    new int[] { 1, 2, 3, 4 },
+    new int[] { 1, 1, 1, 3,3,4,3,2,4,2 },
+};
+
+foreach (int[] nums in numsAarr)
+{
+    Console.WriteLine(ContainsDuplicate.CheckIfArrayContainsDuplicate(nums));
+}
+
+Console.WriteLine("Reverse LinkedList!");
+
+int[] vals = new int[] { 1, 2, 3, 4, 5 };
+CustomLinkedList<int> linkedList = new CustomLinkedList<int>(vals);
+
+Console.WriteLine("Orginal LinkedList!");
+linkedList.Print();
+
+Console.WriteLine("Reversed LinkedList!");
+linkedList.Reverse();
+linkedList.Print();
+
+
+Console.WriteLine("Reversed LinkedList!");
+int[] vals2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+CustomLinkedList<int> linkedList2 = new CustomLinkedList<int>(vals2);
+var node = ReverseLinkedList<int>.Reverse(linkedList2.First);
+ReverseLinkedList<int>.Print(node);
+
+int[] arr = new int[] {1, 4, 7, 9, 11, 23, 45, 53 };
+int[] indices = TwoSum.Solution(arr, 5);
+int[] indices2 = TwoSum.Solution(arr, 8);
+int[] indices3 = TwoSum.Solution(arr, 13);
+Console.ReadLine();
